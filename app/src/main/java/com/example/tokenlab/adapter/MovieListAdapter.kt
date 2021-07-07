59d59e404562fe3e0cc737fc8a207d4011867221
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-07 15:25:58 -0300

    create class MovieListAdapter and interface OnMovieButtonClickListener

---
+++ app/src/main/java/com/example/tokenlab/adapter/MovieListAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.tokenlab.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tokenlab.R
+import com.example.tokenlab.click.listener.OnMovieButtonClickListener
+import com.example.tokenlab.domains.movie.Movie
+
+class MovieListAdapter(
+    private var movieList: List<Movie>,
+    private var onMovieButtonClickListener: OnMovieButtonClickListener
+) : RecyclerView.Adapter<MovieListAdapter.MovieListViewHolder>() {
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MovieListViewHolder {
+        return MovieListViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_movie,
+                parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MovieListViewHolder, position: Int) {
+        holder.bind(movieList.get(position), onMovieButtonClickListener)
+    }
+
+    override fun getItemCount(): Int {
+        return movieList.size
+    }
+
+    inner class MovieListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val itemMovieImageView: ImageView = itemView.findViewById(R.id.itemMovieImageView)
+        private val itemMovieTitleTextView: TextView = itemView.findViewById(R.id.itemMovieTitleTextView)
+        private val itemMovieDateTextView: TextView = itemView.findViewById(R.id.itemMovieDateTextView)
+        private val itemMovieVoteAverageTextView: TextView = itemView.findViewById(R.id.itemMovieVoteAverageTextView)
+        private val itemMovieButton: Button = itemView.findViewById(R.id.itemMovieButton)
+
+        fun bind(get: Movie, onMovieButtonClickListener: OnMovieButtonClickListener) {
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/tokenlab/click/listener/OnMovieButtonClickListener.kt
@@ -0,0 +1,5 @@
+package com.example.tokenlab.click.listener
+
+interface OnMovieButtonClickListener {
+    fun onClick(movieId: String)
+}
