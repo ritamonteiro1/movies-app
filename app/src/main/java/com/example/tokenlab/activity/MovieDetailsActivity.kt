10581983418bb7392dcd9b7ecf3d2ddeb656507c
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-07 18:49:58 -0300

    add fun findViewsById in MovieDetailsActivity.kt

--- app/src/main/java/com/example/tokenlab/activity/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/tokenlab/activity/MovieDetailsActivity.kt
@@ -2,11 +2,36 @@ package com.example.tokenlab.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tokenlab.R
 
 class MovieDetailsActivity : AppCompatActivity() {
+    private var movieDetailsToolBar: Toolbar? = null
+    private var movieDetailsTitleTextView: TextView? = null
+    private var movieDetailsVoteAverageTextView: TextView? = null
+    private var movieDetailsImageView: ImageView? = null
+    private var movieDetailsReleaseDateTextView: TextView? = null
+    private var movieDetailsDateTextView: TextView? = null
+    private var movieDetailsGenresTextView: TextView? = null
+    private var movieDetailsRecyclerView: RecyclerView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+        findViewsById()
+    }
+
+    private fun findViewsById() {
+        movieDetailsToolBar = findViewById(R.id.movieDetailsToolBar)
+        movieDetailsTitleTextView = findViewById(R.id.movieDetailsTitleTextView)
+        movieDetailsVoteAverageTextView = findViewById(R.id.movieDetailsVoteAverageTextView)
+        movieDetailsImageView = findViewById(R.id.movieDetailsImageView)
+        movieDetailsReleaseDateTextView = findViewById(R.id.movieDetailsReleaseDateTextView)
+        movieDetailsDateTextView = findViewById(R.id.movieDetailsDateTextView)
+        movieDetailsGenresTextView = findViewById(R.id.movieDetailsGenresTextView)
+        movieDetailsRecyclerView = findViewById(R.id.movieDetailsRecyclerView)
     }
 }
