c4e38bff5cd23aa5a88fec4964a5543a29f03af3
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-07 13:19:41 -0300

    create LoginActivity.kt and add fun findViewsById

--- app/src/main/java/com/example/tokenlab/LoginActivity.kt
+++ app/src/main/java/com/example/tokenlab/LoginActivity.kt
@@ -1,12 +1,31 @@
 package com.example.tokenlab
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    private var loginEmailTextInputLayout: TextInputLayout? = null
+    private var loginPasswordTextInputLayout: TextInputLayout? = null
+    private var loginEmailEditText: EditText? = null
+    private var loginPasswordEditText: EditText? = null
+    private var loginButton: Button? = null
+    private var loadingDialog: Dialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        findViewsById()
+    }
+
+    private fun findViewsById() {
+        loginEmailTextInputLayout = findViewById(R.id.loginEmailTextInputLayout)
+        loginPasswordTextInputLayout = findViewById(R.id.loginPasswordTextInputLayout)
+        loginEmailEditText = findViewById(R.id.loginEmailEditText)
+        loginPasswordEditText = findViewById(R.id.loginPasswordEditText)
+        loginButton = findViewById(R.id.loginButton)
     }
 }
