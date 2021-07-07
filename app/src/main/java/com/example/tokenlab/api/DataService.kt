9d69714d09fcc460608514089cca5db41ef21736
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-07 15:54:09 -0300

    create class DataService.kt

--- app/src/main/java/com/example/tokenlab/api/DataService.kt
+++ app/src/main/java/com/example/tokenlab/api/DataService.kt
@@ -1,4 +1,16 @@
 package com.example.tokenlab.api
 
-class DataService {
+import com.example.tokenlab.domains.movie.MovieResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+abstract class DataService {
+    @GET("movies")
+    abstract fun recoverMovieList(): Call<MovieResponse>
+
+    @GET("movies/{id}")
+    abstract fun recoverMovieDetails(
+        @Path("id") id: Int
+    ): Call<MovieResponse>
 }
