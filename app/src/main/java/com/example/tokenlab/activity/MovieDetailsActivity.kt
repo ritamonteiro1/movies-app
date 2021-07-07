eead1f076b4ac5cbc4c2f4e80eca47ea4072621a
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-07 19:39:35 -0300

    add fun getClickedMovieDetailsFromApi in MovieDetailsActivity.kt

--- app/src/main/java/com/example/tokenlab/activity/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/tokenlab/activity/MovieDetailsActivity.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
@@ -12,8 +13,11 @@ import com.example.tokenlab.R
 import com.example.tokenlab.api.Api
 import com.example.tokenlab.api.DataService
 import com.example.tokenlab.constants.Constants
+import com.example.tokenlab.domains.movie.Movie
 import com.example.tokenlab.domains.movie.MovieResponse
 import com.example.tokenlab.extensions.createLoadingDialog
+import com.example.tokenlab.extensions.downloadImage
+import com.example.tokenlab.extensions.showErrorDialog
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -34,6 +38,7 @@ class MovieDetailsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_movie_details)
         findViewsById()
         loadingDialog = this.createLoadingDialog()
+        loadingDialog?.show()
         setupToolBar()
         val clickedMovieId = retrieverClickedMovieId()
         getClickedMovieDetailsFromApi(clickedMovieId)
@@ -44,15 +49,63 @@ class MovieDetailsActivity : AppCompatActivity() {
         val call: Call<MovieResponse> = dataService.recoverMovieDetails(clickedMovieId)
         call.enqueue(object : Callback<MovieResponse> {
             override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
-                TODO("Not yet implemented")
+                loadingDialog?.dismiss()
+                if (response.isSuccessful && response.body() != null) {
+                    val movieResponse = response.body()
+                    val clickedMovie = getClickedMovie(movieResponse)
+                    val movieGenres: List<String> = movieResponse?.genres.orEmpty()
+                    showClickedMovieDetails(clickedMovie, movieGenres)
+                    setVisibilityVisibleViews()
+                } else {
+                    setVisibilityGoneViews()
+                    this@MovieDetailsActivity.showErrorDialog(getString(R.string.occurred_error))
+                }
             }
 
             override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-                TODO("Not yet implemented")
+                loadingDialog?.dismiss()
+                setVisibilityGoneViews()
+                this@MovieDetailsActivity.showErrorDialog(getString(R.string.error_connection_fail))
             }
 
         })
     }
+
+    private fun setVisibilityVisibleViews() {
+        movieDetailsImageView?.visibility = View.VISIBLE
+        movieDetailsTitleTextView?.visibility = View.VISIBLE
+        movieDetailsVoteAverageTextView?.visibility = View.VISIBLE
+        movieDetailsReleaseDateTextView?.visibility = View.VISIBLE
+        movieDetailsDateTextView?.visibility = View.VISIBLE
+        movieDetailsGenresTextView?.visibility = View.VISIBLE
+        movieDetailsRecyclerView?.visibility = View.VISIBLE
+    }
+
+    private fun setVisibilityGoneViews() {
+        movieDetailsImageView?.visibility = View.GONE
+        movieDetailsTitleTextView?.visibility = View.GONE
+        movieDetailsVoteAverageTextView?.visibility = View.GONE
+        movieDetailsReleaseDateTextView?.visibility = View.GONE
+        movieDetailsDateTextView?.visibility = View.GONE
+        movieDetailsGenresTextView?.visibility = View.GONE
+        movieDetailsRecyclerView?.visibility = View.GONE
+    }
+
+    private fun showClickedMovieDetails(clickedMovie: Movie, movieGenres: List<String>) {
+        movieDetailsTitleTextView?.text = clickedMovie.title
+        movieDetailsVoteAverageTextView?.text = clickedMovie.voteAverage.toString()
+        movieDetailsImageView?.downloadImage(clickedMovie.imageUrl)
+        movieDetailsDateTextView?.text = clickedMovie.releaseDate
+    }
+
+    private fun getClickedMovie(movieResponse: MovieResponse?) =
+        Movie(
+            movieResponse?.id ?: Constants.NULL_INT_RESPONSE,
+            movieResponse?.voteAverage ?: Constants.NULL_DOUBLE_RESPONSE,
+            movieResponse?.title.orEmpty(),
+            movieResponse?.imageUrl.orEmpty(),
+            movieResponse?.releaseDate.orEmpty()
+        )
 
     private fun retrieverClickedMovieId(): Int {
         return intent.getSerializableExtra(Constants.ID_MOVIE) as Int
@@ -68,6 +121,7 @@ class MovieDetailsActivity : AppCompatActivity() {
 
     private fun setupToolBar() {
         setSupportActionBar(movieDetailsToolBar)
+        supportActionBar?.title = Constants.EMPTY
     }
 
     private fun findViewsById() {
