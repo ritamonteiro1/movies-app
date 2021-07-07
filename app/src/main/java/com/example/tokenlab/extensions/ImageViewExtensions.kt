f254de1af0b9b27d89a02b693c06e59e4b2a70ee
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-07 15:27:19 -0300

    create ImageViewExtensions.kt

---
+++ app/src/main/java/com/example/tokenlab/extensions/ImageViewExtensions.kt
@@ -0,0 +1,8 @@
+package com.example.tokenlab.extensions
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+fun ImageView.downloadImage(baseImageUrl: String) {
+    Glide.with(context).load(baseImageUrl).into(this)
+}
