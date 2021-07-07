9f9f5ed5752ce3681aacb46369ab879a4493087d
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-07 13:51:55 -0300

    add setupLoginButton in LoginActivity.kt

--- app/src/main/java/com/example/tokenlab/activity/LoginActivity.kt
+++ app/src/main/java/com/example/tokenlab/activity/LoginActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import com.example.tokenlab.R
+import com.example.tokenlab.domains.EmailStatus
+import com.example.tokenlab.domains.User
 import com.example.tokenlab.extensions.createLoadingDialog
 import com.google.android.material.textfield.TextInputLayout
 
@@ -22,7 +24,32 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         findViewsById()
         loadingDialog = this.createLoadingDialog()
+        setupLoginButton()
     }
+
+    private fun setupLoginButton() {
+        loginButton?.setOnClickListener {
+            val user = createUser()
+            val isValidUserEmail = user.isValidEmail()
+            val isValidUserPassword = user.isValidPassword()
+            treatInvalidUserEmail(isValidUserEmail)
+            treatInvalidUserPassword(isValidUserPassword)
+
+        }
+    }
+
+    private fun treatInvalidUserPassword(validUserPassword: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    private fun treatInvalidUserEmail(validUserEmail: EmailStatus) {
+        TODO("Not yet implemented")
+    }
+
+    private fun createUser() = User(
+        loginEmailEditText?.text?.toString().orEmpty(),
+        loginPasswordEditText?.text?.toString().orEmpty()
+    )
 
     private fun findViewsById() {
         loginEmailTextInputLayout = findViewById(R.id.loginEmailTextInputLayout)
