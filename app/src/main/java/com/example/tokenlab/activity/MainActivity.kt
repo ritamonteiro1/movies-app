13081eee1125d4d908730a090724fc16af006ab5
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-07 14:51:36 -0300

    create MainActivity.kt and add fun findViewsById

--- app/src/main/java/com/example/tokenlab/activity/MainActivity.kt
+++ app/src/main/java/com/example/tokenlab/activity/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.tokenlab.activity
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tokenlab.R
 
 class MainActivity : AppCompatActivity() {
+    private var mainToolBar: Toolbar? = null
+    private var mainRecyclerView: RecyclerView? = null
+    private var loadingDialog: Dialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewsById()
+    }
+
+    private fun findViewsById() {
+        mainToolBar = findViewById(R.id.mainToolBar)
+        mainRecyclerView = findViewById(R.id.mainRecyclerView)
     }
 }
