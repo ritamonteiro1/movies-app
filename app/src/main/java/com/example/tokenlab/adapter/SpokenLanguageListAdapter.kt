79d6a959c5009fe51a9cd73078463f780e394c84
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-08 20:44:51 -0300

    create item_spoken_language.xml and SpokenLanguageListAdapter

---
+++ app/src/main/java/com/example/tokenlab/adapter/SpokenLanguageListAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.tokenlab.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tokenlab.R
+import com.example.tokenlab.domains.movie.details.spoken.language.SpokenLanguage
+
+class SpokenLanguageListAdapter(var spokenLanguageList: List<SpokenLanguage>) :
+    RecyclerView.Adapter<SpokenLanguageListAdapter.SpokenListViewHolder>() {
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SpokenListViewHolder {
+        return SpokenListViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_spoken_language,
+                parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: SpokenListViewHolder,
+        position: Int
+    ) {
+        holder.bind(spokenLanguageList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return spokenLanguageList.size
+    }
+
+    inner class SpokenListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var itemSpokenLanguageTextView: TextView =
+            itemView.findViewById(R.id.itemSpokenLanguageTextView)
+
+        fun bind(spokenLanguage: SpokenLanguage) {
+            itemSpokenLanguageTextView.text = spokenLanguage.name
+        }
+    }
+}
