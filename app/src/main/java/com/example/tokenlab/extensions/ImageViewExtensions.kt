c58ddd58c1b358cb691c33211a251d887f7a83c6
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-08 11:45:56 -0300

    change in extension fun downloadImage in ImageViewExtensions.kt

--- app/src/main/java/com/example/tokenlab/extensions/ImageViewExtensions.kt
+++ app/src/main/java/com/example/tokenlab/extensions/ImageViewExtensions.kt
@@ -2,8 +2,9 @@ package com.example.tokenlab.extensions
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.example.tokenlab.R
 
 
 fun ImageView.downloadImage(baseImageUrl: String) {
-        Glide.with(context).load(baseImageUrl).into(this)
+    Glide.with(context).load(baseImageUrl).error(R.drawable.img_error).into(this)
 }
