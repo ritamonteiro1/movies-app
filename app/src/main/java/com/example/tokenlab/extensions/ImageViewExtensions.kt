5a55196d355ac45b473f1c28fb09945a14c251d7
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-08 11:31:05 -0300

    add img_error.png in drawable

--- app/src/main/java/com/example/tokenlab/extensions/ImageViewExtensions.kt
+++ app/src/main/java/com/example/tokenlab/extensions/ImageViewExtensions.kt
@@ -3,6 +3,7 @@ package com.example.tokenlab.extensions
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 
+
 fun ImageView.downloadImage(baseImageUrl: String) {
-    Glide.with(context).load(baseImageUrl).into(this)
+        Glide.with(context).load(baseImageUrl).into(this)
 }
--- app/src/main/java/com/example/tokenlab/extensions/StringExtensions.kt
+++ app/src/main/java/com/example/tokenlab/extensions/StringExtensions.kt
@@ -2,6 +2,7 @@ package com.example.tokenlab.extensions
 
 import android.util.Patterns
 
+
 fun String.isValidEmail(): Boolean {
     return this.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
 }
