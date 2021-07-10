43f1aec42ccef5379960c7ab3809aeb91caa61f8
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-10 00:10:33 -0300

    remove SerializedName in data class MovieDetails.kt

--- app/src/main/java/com/example/tokenlab/domains/movie.details/details/MovieDetails.kt
+++ app/src/main/java/com/example/tokenlab/domains/movie.details/details/MovieDetails.kt
@@ -1,15 +1,14 @@
 package com.example.tokenlab.domains.movie.details.details
 
-import com.google.gson.annotations.SerializedName
 
 data class MovieDetails(
     val title: String,
-    @SerializedName("vote_average") val voteAverage: Double,
-    @SerializedName("vote_count") val voteCount: Int,
-    @SerializedName("release_date") val releaseDate: String,
-    @SerializedName("poster_url") val imageUrl: String,
-    @SerializedName("original_language") val originalLanguage: String,
-    @SerializedName("original_title") val originalTitle: String,
+    val voteAverage: Double,
+    val voteCount: Int,
+    val releaseDate: String,
+    val imageUrl: String,
+    val originalLanguage: String,
+    val originalTitle: String,
     val tagline: String,
     val belongsToCollection: String
 )
