aa70b8b03e553f6295b8ac0a07921d93421543ab
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-10 11:59:50 -0300

    create MovieDetailsList.kt

---
+++ app/src/main/java/com/example/tokenlab/adapter/MovieDetailsListAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.tokenlab.adapter
+
+import com.example.tokenlab.domains.movie.details.movie.details.list.MovieDetailsList
+
+class MovieDetailsListAdapter(private val movieDetailsList: List<MovieDetailsList>) {
+}
---
+++ app/src/main/java/com/example/tokenlab/domains/movie.details/movie.details.list/MovieDetailsList.kt
@@ -0,0 +1,3 @@
+package com.example.tokenlab.domains.movie.details.movie.details.list
+
+data class MovieDetailsList(private val movieDetailsType: String, private val movieDetailsValue: String)
